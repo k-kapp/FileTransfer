ca014d224819bec6efb9ec1202c0bdfef64069b8
Konrad Kapp <k_kapp@yahoo.com>
2017-03-26 02:08:00 +0200

    Fix commands

--- FileTransfer/Client.cpp
+++ FileTransfer/Client.cpp
@@ -14,38 +14,35 @@
 #include <boost/filesystem.hpp>
 
 /*
-TODO: 
- - make connect and transfer commands, instead of entering <ip> natively
+fine-tune general interface
 */
 
 using namespace boost::asio::ip;
 using namespace boost::asio;
 
-//static boost::asio::io_service service_ptr;
 static std::unique_ptr<io_service> service_ptr;
 static std::unique_ptr<tcp::socket> sock_ptr;
 
-//static tcp::endpoint ep(address::from_string("127.0.0.1"), 8001);
-//static tcp::endpoint ep(address::from_string("192.168.1.7"), 8001);
 static tcp::endpoint ep;
 
 static std::list<std::string> filepaths;
 
 static std::mutex filepaths_lock;
 static std::mutex wait_transfer;
+static std::mutex socket_lock;
 static volatile bool quit = false;
 static volatile bool connected = false;
 
 void connect_timeout(boost::system::error_code &connect_ec, const boost::system::error_code &ec);
 void async_connect_done(boost::system::error_code &timout_ec, const boost::system::error_code &ec);
-//void connect_timeout(const boost::system::error_code &ec);
-//void async_connect_done(const boost::system::error_code &ec);
 void connect_server();
 void console_listener();
 void send_listener();
 void incoming_listener();
+void add_filepath(const std::string &filepath);
 
 int handle_commands(std::string cmd);
+bool connect_to_server(std::string ip_str, std::string port_str);
 
 static std::string cwd = boost::filesystem::current_path().string();
 
@@ -55,18 +52,24 @@ enum commands_enum
 	CWD,
 	DIR,
 	HELP,
+	CONNECT,
+	TRANSFER,
 	EMPTY
 };
 
-std::array<std::string, 4> commands = { "exit", "cwd", "dir", "help"};
+std::array<std::string, 6> commands = { "exit", "cwd", "dir", "help", "connect", "transfer"};
 
 template<typename T>
-void print_vec(T vec)
+void print_vec(T vec, bool new_line = false)
 {
 	using namespace std;
-	for (auto& val : vec)
+	for (auto iter = vec.begin(); iter != vec.end(); advance(iter, 1))
 	{
-		cout << val << " ";
+		cout << *iter;
+		if (next(iter, 1) != vec.end())
+			cout << ", ";
+		if (new_line)
+			cout << endl;
 	}
 	cout << endl;
 }
@@ -80,131 +83,166 @@ int main()
 
 	service_ptr = make_unique<io_service>();
 
-	cout << "Enter <Host ip address> <Port>. Type 'exit' to quit" << endl;
-
 	while (!quit)
 	{
-		string ip_addr;
-		string port_num_str;
-		string server_info;
+		std::thread console_thread(console_listener);
+		std::thread send_thread(send_listener);
+		std::thread incoming_thread(incoming_listener);
 
-		int port_num;
+		console_thread.join();
+		send_thread.join();
+		incoming_thread.join();
 
-		cout << "> ";
-		std::getline(std::cin, server_info);
-
-		int command_result = handle_commands(server_info);
-
-		if (command_result != -1)
-			continue;
-
-		string::iterator str_iter = server_info.begin();
-		while (str_iter != server_info.end() && std::isgraph(*str_iter))
-		{
-			ip_addr += *str_iter;
-			advance(str_iter, 1);
-		}
-
-		if (str_iter != server_info.end() && !isgraph(*str_iter))
-			advance(str_iter, 1);
-
-		while (str_iter != server_info.end())
-		{
-			if (!isgraph(*str_iter))
-			{
-				continue;
-			}
-			else if (isalpha(*str_iter))
-			{
-				cout << "<Port> parameter must be a number" << endl;
-				continue;
-			}
-			port_num_str += *str_iter;
-			advance(str_iter, 1);
-		}
-
-		port_num = atoi(port_num_str.c_str());
-
-
-		if (port_num < 0)
-		{
-			cout << "Invalid port number" << endl;
-			continue;
-		}
-
-		ep.address(address::from_string(ip_addr));
-		ep.port(port_num);
-
-		try
-		{
-			boost::system::error_code timeout_ec, connect_ec;
-			timeout_ec = connect_ec = boost::asio::error::would_block;
-
-			sock_ptr = make_unique<tcp::socket>(*service_ptr);
-
-			deadline_timer timer(*service_ptr);
-			timer.expires_from_now(boost::posix_time::seconds(3));
-
-			auto bind_timer = bind(connect_timeout, std::ref(timeout_ec), std::placeholders::_1);
-			auto bind_connect = bind(async_connect_done, std::ref(connect_ec), std::placeholders::_1);
-
-			timer.async_wait(bind_timer);
-			sock_ptr->async_connect(ep, bind_connect);
-
-			while ((timeout_ec == error::would_block) && (connect_ec == error::would_block))
-			{
-				service_ptr->run_one();
-			}
-
-			cout << "current seconds: " << boost::posix_time::second_clock::local_time().time_of_day().seconds() << endl;
-			cout << "timer cancels at:  " << timer.expires_at().time_of_day().seconds() << endl;
-			timer.cancel();
-
-			if (timeout_ec != error::would_block)
-			{
-				sock_ptr->close();
-				sock_ptr.reset();
-				service_ptr->stop();
-				service_ptr.reset();
-				service_ptr = make_unique<io_service>();
-				cout << "Connection to server timed out" << endl;
-				continue;
-			}
-			if (connect_ec)
-			{
-				sock_ptr->close();
-				sock_ptr.reset();
-				service_ptr->stop();
-				service_ptr.reset();
-				service_ptr = make_unique<io_service>();
-				cout << "Error connecting to server" << endl;
-				continue;
-			}
-
-			cout << "Connected to server." << endl;
-
-			socket_base::keep_alive option(true);
-			sock_ptr->set_option(option);
-
-			std::thread console_thread(console_listener);
-			std::thread send_thread(send_listener);
-			std::thread incoming_thread(incoming_listener);
-
-			console_thread.join();
-			send_thread.join();
-			incoming_thread.join();
-			break;
-		}
+		/*
 		catch (boost::system::system_error e)
 		{
 			std::cout << e.what() << std::endl;
 			std::cout << "Press any key to continue..." << std::endl;
 			getc(stdin);
 		}
+		*/
 	}
 
-
     return 0;
+}
+
+bool ip_valid(std::string ip_str)
+{
+	using namespace std;
+
+	stringstream stream;
+
+	vector<string> ip_split;
+
+	boost::split(ip_split, ip_str, [](const char c) {return c == '.'; });
+
+	if (ip_split.size() != 4)
+	{
+		cout << "Invalid format for ip address" << endl;
+	}
+
+	int ip_num;
+	for (auto& num_str : ip_split)
+	{
+		stream.str("");
+		stream.clear();
+		stream << num_str;
+		stream >> ip_num;
+		if (!stream.eof())
+		{
+			cout << "Invalid format for IP address" << endl;
+			return false;
+		}
+		if (ip_num < 0 && ip_num > 255)
+		{
+			cout << "Numbers in IP address must be between 0 and 255, inclusive" << endl;
+			return false;
+		}
+	}
+	return true;
+}
+
+bool port_valid(std::string port_str)
+{
+	using namespace std;
+
+	stringstream stream;
+
+	vector<string> ip_split;
+
+	int port_val;
+
+	stream << port_str;
+	stream >> port_val;
+
+	if (!stream.eof() || port_val < 0)
+	{
+		cout << "Invalid format for port number" << endl;
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+bool connect_to_server(std::string ip_str, std::string port_str)
+{
+	using namespace std;
+
+	if (!ip_valid(ip_str))
+		return false;
+	if (!port_valid(port_str))
+		return false;
+
+	unsigned port_num = atoi(port_str.c_str());
+
+
+	ep.address(address::from_string(ip_str));
+	ep.port(port_num);
+
+	try
+	{
+		boost::system::error_code timeout_ec, connect_ec;
+		timeout_ec = connect_ec = boost::asio::error::would_block;
+
+		socket_lock.lock();
+		sock_ptr = make_unique<tcp::socket>(*service_ptr);
+
+		deadline_timer timer(*service_ptr);
+		timer.expires_from_now(boost::posix_time::seconds(3));
+
+		auto bind_timer = bind(connect_timeout, std::ref(timeout_ec), std::placeholders::_1);
+		auto bind_connect = bind(async_connect_done, std::ref(connect_ec), std::placeholders::_1);
+
+		timer.async_wait(bind_timer);
+		sock_ptr->async_connect(ep, bind_connect);
+
+		while ((timeout_ec == error::would_block) && (connect_ec == error::would_block))
+		{
+			service_ptr->run_one();
+		}
+
+		cout << "current seconds: " << boost::posix_time::second_clock::local_time().time_of_day().seconds() << endl;
+		cout << "timer cancels at:  " << timer.expires_at().time_of_day().seconds() << endl;
+		timer.cancel();
+
+		if (timeout_ec != error::would_block)
+		{
+			sock_ptr->close();
+			sock_ptr.reset();
+			service_ptr->stop();
+			service_ptr.reset();
+			service_ptr = make_unique<io_service>();
+			socket_lock.unlock();
+			cout << "Connection to server timed out" << endl;
+			return false;
+		}
+		if (connect_ec)
+		{
+			sock_ptr->close();
+			sock_ptr.reset();
+			service_ptr->stop();
+			service_ptr.reset();
+			service_ptr = make_unique<io_service>();
+			socket_lock.unlock();
+			cout << "Error connecting to server" << endl;
+			return false;
+		}
+
+		socket_base::keep_alive option(true);
+		sock_ptr->set_option(option);
+
+	}
+	catch (boost::system::system_error e)
+	{
+		std::cout << e.what() << std::endl;
+		std::cout << "Press any key to continue..." << std::endl;
+		getc(stdin);
+	}
+
+	socket_lock.unlock();
+	return true;
 }
 
 void print_curr_dir()
@@ -275,12 +313,8 @@ void list_files()
 	}
 }
 
-int handle_commands(std::string command)
+std::vector<std::string> parse_command_no_quote(std::string command)
 {
-	/*
-	TODO: provide for directory names that have spaces in 
-	*/
-
 	using namespace std;
 
 	vector<string> words;
@@ -288,25 +322,118 @@ int handle_commands(std::string command)
 	boost::trim_left(command);
 	boost::trim_right(command);
 
-	boost::split(words, command, [](const char &c) {return c == ' '; }, boost::token_compress_on);
-	if ((words.front() == "cwd" || words.front() == "ccwd") && words.size() > 1)
+	if (command.size() == 0)
 	{
-		vector<string> new_vec(next(words.begin(), 1), words.end());
-		string candidate_dir = boost::algorithm::join(new_vec, " ");
-		words.erase(next(words.begin(), 1), words.end());
-		words.push_back(candidate_dir);
+		return{};
 	}
 
+	boost::split(words, command, [](const char &c) {return c == ' '; }, boost::token_compress_on);
+
+	return words;
+}
+
+std::vector<std::string> combine_quotes(std::string command)
+{
+	using namespace std;
+
+	std::vector<std::string> words;
+
+	string curr_word = "";
+	bool in_quotes = false;
+
+	for (auto iter = command.begin(); iter != command.end(); advance(iter, 1))
+	{
+		if (*iter == '"')
+		{
+			if (in_quotes)
+			{
+				curr_word += *iter;
+				if (curr_word.size() > 2)
+					words.push_back(curr_word);
+				curr_word = "";
+			}
+			else
+			{
+				if (curr_word.size() > 0)
+				{
+					words.push_back(curr_word);
+				}
+				curr_word = "\"";
+			}
+			in_quotes = !in_quotes;
+		}
+		else
+			curr_word += *iter;
+	}
+	if (curr_word.size() > 0 && (curr_word.size() != 1 || curr_word.at(0) != '"'))
+		words.push_back(curr_word);
+
+	return words;
+}
+
+std::vector<std::string> parse_command(std::string command)
+{
+	using namespace std;
+
+	vector<string> words = combine_quotes(command);
+
+	vector<unsigned> idxes_replace;
+	for (unsigned i = 0; i < words.size(); i++)
+	{
+		if (words.at(i).size() > 0 && words.at(i).at(0) != '"')
+		{
+			idxes_replace.push_back(i);
+		}
+	}
+
+	reverse(idxes_replace.begin(), idxes_replace.end());
+
+	for (auto i : idxes_replace)
+	{
+		auto new_words_i = parse_command_no_quote(words.at(i));
+		words.erase(next(words.begin(), i));
+		for (auto r_iter = new_words_i.rbegin(); r_iter != new_words_i.rend(); advance(r_iter, 1))
+			words.insert(next(words.begin(), i), *r_iter);
+	}
+
+	for (auto &str : words)
+	{
+		if (str.front() == '"')
+		{
+			str.erase(str.begin());
+		}
+		if (str.back() == '"')
+		{
+			str.erase(next(str.end(), -1));
+		}
+	}
+
+	return words;
+}
+
+
+
+/*
+filepaths_lock must be held when entering this function
+*/
+int handle_commands(std::string command)
+{
+
+	using namespace std;
+
+	vector<string> words = parse_command(command);
+	
 	if (words.size() == 1 && words.front().size() == 0)
 	{
 		return EMPTY;
 	}
 
-	print_vec(words);
-
 	auto found_iter = find(commands.begin(), commands.end(), words.front());
 	if (found_iter == commands.end())
+	{
+		cout << "Unkown command: " << words.front() << endl;
 		return -1;
+	}
 	unsigned idx = distance(commands.begin(), found_iter);
 	switch (idx)
 	{
@@ -348,6 +475,37 @@ int handle_commands(std::string command)
 			cout << "Enter <Host ip address> <Port>. Type 'exit' to quit" << endl;
 		}
 		break;
+		case (CONNECT):
+		{
+			if (words.size() != 3)
+			{
+				cout << "connect command takes two parameters: <ip> <port>" << endl;
+				break;
+			}
+			if (!connect_to_server(words.at(1), words.at(2)))
+			{
+				cout << "Failed to connect to server" << endl;
+			}
+			else
+			{
+				cout << "Successfully connected to server" << endl;
+			}
+		}
+		break;
+		case (TRANSFER):
+		{
+			if (words.size() < 2)
+			{
+				cout << "transfer command needs at least one argument" << endl;
+				break;
+			}
+
+			for (auto iter = next(words.begin(), 1); iter != words.end(); advance(iter, 1))
+			{
+				filepaths.push_back(cwd + *iter);
+			}
+		}
+		break;
 	}
 
 	return idx;
@@ -383,23 +541,30 @@ void console_listener()
 	//getc(stdin);
 	while (!quit)
 	{
+		filepaths_lock.lock();
 		if (filepaths.size() > 0)
+		{
+			filepaths_lock.unlock();
 			continue;
+		}
 		std::string filepath = "";
+		std::cout << "> ";
 		std::getline(std::cin, filepath);
 		//std::cin.ignore(std::numeric_limits<unsigned>().max(), '\n');
 		if (filepath.size() > 0)
 		{
 			int command_result = handle_commands(filepath);
+			/*
 			if (command_result != -1)
 			{
 				continue;
 			}
-
 			filepaths_lock.lock();
 			filepaths.push_back(cwd + filepath);
 			filepaths_lock.unlock();
+			*/
 		}
+		filepaths_lock.unlock();
 	}
 }
 
@@ -407,8 +572,15 @@ void send_listener()
 {
 	while (!quit)
 	{
+		socket_lock.lock();
+		if (!sock_ptr)
+		{
+			socket_lock.unlock();
+			continue;
+		}
+
 		filepaths_lock.lock();
-		if (filepaths.size() > 0)
+		while (filepaths.size() > 0)
 		{
 
 			std::ifstream ifs(filepaths.front(), std::ios::binary);
@@ -418,7 +590,6 @@ void send_listener()
 				std::cout << "Invalid filepath specified: file not found" << std::endl;
 				std::cout << "You entered: |" << filepaths.front() << "|" << std::endl;
 				filepaths.pop_front();
-				filepaths_lock.unlock();
 				continue;
 			}
 
@@ -478,6 +649,7 @@ void send_listener()
 
 			filepaths.pop_front();
 		}
+		socket_lock.unlock();
 		filepaths_lock.unlock();
 	}
 }
@@ -486,11 +658,25 @@ void incoming_listener()
 {
 	while (!quit)
 	{
+		socket_lock.lock();
+		if (!sock_ptr)
+		{
+			socket_lock.unlock();
+			continue;
+		}
 		if (sock_ptr->available())
 		{
 			char read_buf[512];
 			sock_ptr->read_some(buffer(read_buf, 512));
 			std::cout << read_buf << std::endl;
 		}
+		socket_lock.unlock();
 	}
+}
+
+void add_filepath(const std::string &filepath)
+{
+	filepaths_lock.lock();
+	filepaths.push_back(filepath);
+	filepaths_lock.lock();
 }
